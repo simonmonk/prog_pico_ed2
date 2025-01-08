7ee62115b3681e8f6d3fe4cee0299b93552f77eb
simonmonk <srmonk@gmail.com>
2025-01-08 08:47:26 +0000

    fixed pwm in RGB example

--- 08_02_RGB.py
+++ 08_02_RGB.py
@@ -5,6 +5,10 @@ red_ch = PWM(Pin(16))
 green_ch = PWM(Pin(17))
 blue_ch = PWM(Pin(15))
 
+red_ch.freq(1000);
+green_ch.freq(1000);
+blue_ch.freq(1000);
+
 button = Pin(12, Pin.IN, Pin.PULL_UP)
 colors = [[255, 0, 0], [127, 127, 0],[0, 255, 0], [0, 127, 127], [0, 0, 255], [127, 0, 127]]
 
@@ -15,6 +19,7 @@ def set_color(rgb):
 
 index = 0
 set_color(colors[index])
+
 while True:
     if button.value() == 0:
         index +=1
@@ -22,3 +27,4 @@ while True:
             index = 0
         sleep(0.2)
     set_color(colors[index])
+
